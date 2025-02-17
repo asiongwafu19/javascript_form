90d3e1db28c5d585d1938e778975bd5f78f45711
COMLAB <jvmayo23-0406@cca.edu.ph>
2025-02-17 17:51:42 +0800

    Javaform
    form na java pa

---
+++ index.php
@@ -0,0 +1,19 @@
+<html> 
+	<head>
+		<title>Javascript Form</title>
+	</head>
+	
+	<body style="background-color: black; color: #FFF;">
+	<h1>asiong pogi</h1>
+		<table id="student_table">
+		</table>
+		
+		<script>
+			const studentTable= document
+			.getElementById("student_table");
+			
+			console.log(studentTable);
+			</script>
+	</body>
+</html>
+
