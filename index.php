6362454588bb75815c0a2f7541d7174c3d01f112
COMLAB <jvmayo23-0406@cca.edu.ph>
2025-02-17 18:34:51 +0800

    new form
    may nangyayare na

--- index.php
+++ index.php
@@ -11,9 +11,38 @@
 		<script>
 			const studentTable= document
 			.getElementById("student_table");
-			
+				
 			console.log(studentTable);
-			</script>
+			const row1 = studentTable.insertRow();
+			
+			const row1col1 = row1.insertCell();
+			const row1col2 = row1.insertCell();
+			
+			row1col1.innerHTML = "LAST NAME";
+			row1col2.innerHTML = "FIRST NAME";
+			
+			const row2 = studentTable.insertRow();
+			const row2col1 = row2.insertCell();
+			const row2col2 = row2.insertCell();
+			
+			const inputLastName = document.createElement("Input");
+			inputLastName.type="text";
+			inputLastName.placeholder="Inster Last Name";
+			
+			row2col1.append (inputLastName);
+			
+			const inputFirstName = document.createElement("input");
+			inputFirstName.type="text";
+			inputFirstName.placeholder="Insert First Name";
+			
+			row2col2.append (inputFirstName);
+			
+			const row3 = studentTable.insertRow();
+			const row3col1 = row3.insertCell();
+			row3col1.colSpan="2";
+			row3col1.innerHTML ="TEST";
+			
+		</script>
 	</body>
 </html>
 
